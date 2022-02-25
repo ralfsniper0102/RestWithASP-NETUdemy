718b3a90d7b449d3c843f2d6fb9ea855b59521a6
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2022-02-25 13:21:28 -0300

    ok

---
+++ 03_RestWithASPNETUdemy_ScaffoldViaVisualStudio/ScaffoldViaVisualStudio/ScaffoldViaVisualStudio/Services/IPersonServices.cs
@@ -0,0 +1,15 @@
+﻿using ScaffoldViaVisualStudio.Controllers.Model;
+using System.Collections.Generic;
+
+namespace ScaffoldViaVisualStudio.Services
+{
+    public interface IPersonServices
+    {
+        Person Create(Person person);
+        void Delete(long id);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+
+    }
+}
---
+++ 03_RestWithASPNETUdemy_ScaffoldViaVisualStudio/ScaffoldViaVisualStudio/ScaffoldViaVisualStudio/Services/Implamatation/PersonServiceImplamentation.cs
@@ -0,0 +1,69 @@
+﻿using ScaffoldViaVisualStudio.Controllers.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ScaffoldViaVisualStudio.Services.Implamatation
+{
+    public class PersonServiceImplamentation : IPersonServices
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Leandro",
+                LastName = "Costa",
+                Address = "Uberllandia - Minas Gerais - Brasil",
+                Gender = "Male"
+            };
+
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name" + 1,
+                LastName = "Person LastName" + 1,
+                Address = "Some Address" + 1,
+                Gender = "Male"
+            };
+ 
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
